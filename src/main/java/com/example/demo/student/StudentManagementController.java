1fc472e3b6e3cd7a01187f6cba6a26dfadc8c1e9
Bence <borsi92@gmail.com>
2020-11-19 22:53:37 +0100

    finished basic authentication section

---
+++ src/main/java/com/example/demo/student/StudentManagementController.java
@@ -0,0 +1,43 @@
+package com.example.demo.student;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+    @RequestMapping("management/api/v1/students")
+public class StudentManagementController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "James Bond"),
+            new Student(2, "Maria Jones"),
+            new Student(3, "Anna Smith")
+    );
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
+    public List<Student> getAllStudents(){
+        return STUDENTS;
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyAuthority('student:write')")
+    public void registerNewStudent(@RequestBody Student student){
+        System.out.println(student);
+        //STUDENTS.add(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAnyAuthority('student:write')")
+    public void deleteStudent(@PathVariable Integer studentId){
+        System.out.println(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAnyAuthority('student:write')")
+    public void updateStudent(@PathVariable Integer studentId, @RequestBody Student student){
+        System.out.println(String.format("%s %s", studentId, student));
+    }
+}
